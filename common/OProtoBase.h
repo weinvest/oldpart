75b9523cd26e609bf64907c90844e63cb471a38e
shugan.li <shugan.li@dfctec.com>
2018-10-15 19:01:17 +0800

    [Normal]开始写serialize的test case

--- common/OProtoBase.h
+++ common/OProtoBase.h
@@ -78,6 +78,10 @@ private:
 
 #define PROTO_FIELDS(typeNamePair) BOOST_PP_SEQ_FOR_EACH_I(PROTO_EXPAND_FIELD, ~, typeNamePair)\
     void Write(Coro::push_type& yield, std::shared_ptr<uint8_t> buf, int32_t offset) override\
+    void Read(Coro::pull_type& source, std::shared_ptr<uint8_t> buf, int32_t offset) override
+
+#define PROTO_IMPLEMENTATION(typeNamePair)
+    void Write(Coro::push_type& yield, std::shared_ptr<uint8_t> buf, int32_t offset) override\
     {\
         BOOST_PP_SEQ_FOR_EACH_I(PROTO_WRITE_FIELD, ~, typeNamePair)\
         yield(std::make_tuple(buf, offset, 0));\
