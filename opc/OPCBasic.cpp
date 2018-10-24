b46091934e11c58b8f17b81aff217e09595fde58
shugan.li <shugan.li@dfctec.com>
2018-10-24 23:11:07 +0800

    [Normal]begin to implement FindFiles

--- opc/OPCBasic.cpp
+++ opc/OPCBasic.cpp
@@ -2,7 +2,8 @@
 #include <boost/filesystem.hpp>
 #include "common/OProtoResponseGuard.h"
 using fs = boost::filesystem;
-void OPCBasic::OnListFilesRequest(const std::shared_ptr<OMessage>& pMessage, ListFilesRequest* pListFilesRequest)
+void OPCBasic::OnListFilesRequest(const std::shared_ptr<OMessage>& pMessage
+    , ListFilesRequest* pListFilesRequest)
 {
     OProtoResponseGuard responseGuard(pMessage, mSocket);
     auto pResponse = responseGuard.Create<ListFilesResponse>();
@@ -47,17 +48,21 @@ void OPCBasic::OnListFilesRequest(const std::shared_ptr<OMessage>& pMessage, Lis
     }
 }
 
-void OPCBasic::OnFindFilesRequest(const std::shared_ptr<OMessage>& pMessage, FindFilesRequest* pFindFilesRequest)
+void OPCBasic::OnFindFilesRequest(const std::shared_ptr<OMessage>& pMessage
+    , FindFilesRequest* pFindFilesRequest)
+{
+    OProtoResponseGuard responseGuard(pMessage, mSocket);
+    auto pResponse = responseGuard.Create<ListFilesResponse>();
+}
+
+void OPCBasic::OnCatFileRequest(const std::shared_ptr<OMessage>& pMessage
+    , CatFileRequest* pCatFileRequest)
 {
 
 }
 
-void OPCBasic::OnCatFileRequest(const std::shared_ptr<OMessage>& pMessage, CatFileRequest* pCatFileRequest)
-{
-
-}
-
-void OPCBasic::OnGrepFileRequest(const std::shared_ptr<OMessage>& pMessage, CatFileRequest* pGrepFileRequest)
+void OPCBasic::OnGrepFileRequest(const std::shared_ptr<OMessage>& pMessage
+    , CatFileRequest* pGrepFileRequest)
 {
 
 }
